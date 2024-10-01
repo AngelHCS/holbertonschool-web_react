5f935390b47c0bc01607723cd7aa9fc6c43f81b6
AngelHCS <159042316+AngelHCS@users.noreply.github.com>
2024-10-01 00:15:53 -0400

    Delete task_0 directory (wrong place)

--- task_0/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-holbertonschool-web_react/task_0$ module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-    ],
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
